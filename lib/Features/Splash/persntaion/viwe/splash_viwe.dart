a33c476b69b095f40c4db7ed359bd291ab9e0ddc
Mohamed <mohmedabrhem981@gmail.com>
2026-04-19 15:37:57 +0200

    careat splash

---
+++ lib/Features/Splash/persntaion/viwe/splash_viwe.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_app/Features/Splash/persntaion/viwe/widght/Splash_viwe_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashViwe extends StatelessWidget {
+  const SplashViwe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: SplashViweBody());
+  }
+}
